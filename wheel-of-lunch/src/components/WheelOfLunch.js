0e6a2165d99e5290ac53b50f3184f604aeef89df
Jeremy the Lakey <jeremy@sla.dev>
2025-05-14 08:58:30 -0500

    scroll to bottom of document

--- wheel-of-lunch/src/components/WheelOfLunch.js
+++ wheel-of-lunch/src/components/WheelOfLunch.js
@@ -408,8 +408,8 @@ const WheelOfLunch = () => {
         setSelectedRestaurant(restaurants[randomIndex]);
         setStatus(`Your lunch destination: ${restaurants[randomIndex].name}`);
         // Scroll to results card
-        if (resultsRef.current) {
-          resultsRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        if (document.documentElement) {
+          document.documentElement.scrollTo({ top: document.documentElement.scrollHeight, behavior: 'smooth' });
         }
       }
     };
